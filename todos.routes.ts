21964a30b3570d4f3e492ac9413b9578f73a975b
asimsaleem-cmyk <asim.saleem@devntech.com>
2025-08-15 11:40:02 +0500

    Update todos.routes.ts

--- todos.routes.ts
+++ todos.routes.ts
@@ -1,5 +1,11 @@
 import { Router } from "express";
-import { fetchTodos, addTodo, editTodo, removeTodo } from "./todos.controllers";
+import {
+  fetchTodos,
+  addTodo,
+  editTodo,
+  removeTodo,
+  markTodoAsDone,
+} from "./todos.controllers";
 
 const router = Router();
 
@@ -7,5 +13,7 @@ router.get("/todos", fetchTodos); // GET all
 router.post("/todos", addTodo); // POST create
 router.put("/todos/:id", editTodo); // PUT update
 router.delete("/todos/:id", removeTodo); // DELETE remove
+router.patch("/todos/:id/done", markTodoAsDone); // PATCH mark as done
 
 export default router;
+
